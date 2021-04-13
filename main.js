5ec455626d4eaecaa568da2573154e6314f22449
Leuteris Mitilineos <std145287@ac.eap.gr>
2021-04-13 22:29:10 +0300

    Update main.js

--- main.js
+++ main.js
@@ -190,17 +190,26 @@ function addPoints(data) {
     });
     // COMMENT UNTIL HERE TO DISABLE SIDEBAR FOR THE MARKERS
 
-    // AwesomeMarkers is used to create fancier icons
-    let icon = L.AwesomeMarkers.icon({
-      icon: "info-circle",
-      iconColor: "white",
-      markerColor: data[row].color,
-      prefix: "fa",
-      extraClasses: "fa-rotate-0",
+    // Custom Icon 
+    let icon = L.icon({
+      iconUrl: './pic/church-map-icon.png',
+      iconSize: [60, 60] // size of the icon
     });
-    if (!markerType.includes("circle")) {
-      marker.setIcon(icon);
-    }
+    
+    marker.setIcon(icon);
+    
+    // AwesomeMarkers is used to create fancier icon 
+    // let icon = L.AwesomeMarkers.icon({
+    //  icon: "info-circle",
+    //  iconColor: "white",
+    //  markerColor: data[row].color,
+    //  prefix: "fa",
+    //  extraClasses: "fa-rotate-0",
+    //});
+    //if (!markerType.includes("circle")) {
+    //  marker.setIcon(icon);
+    //}
+     
   }
 }
 
